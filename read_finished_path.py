68d0e4a2b48b537483dacec7a18bfdd208b89873
Mussil <79862052+Mussil@users.noreply.github.com>
2021-12-22 16:58:55 +0200

    Separate the graph creation into a separate file

--- read_finished_path.py
+++ read_finished_path.py
@@ -2,49 +2,13 @@ from matplotlib import pyplot as plt
 from urllib.parse import unquote
 import networkx as nx
 
-# ####################create the graph##################
+#our files
+from createGraph import createGraph
 
-def createNodes(g,Lines):
-    for line in Lines[12:]:
-        article=line.replace('\n', '')
-        article = unquote(article)
-        g.add_node(article,category=[])
-    return g
 
-def createEdges(g,Lines):
-    for line in Lines[12:]:
-        line=line.replace('\n', '')
-        nodeOne,nodeTwo=line.split('\t')
-        nodeOne = unquote(nodeOne)
-        nodeTwo = unquote(nodeTwo)
-        g.add_edge(nodeOne,nodeTwo)
-    return g
 
-def addCategory(g,Lines):
-    for line in Lines[13:]:
-        line=line.replace('\n', '')
-        node,category=line.split('\t')
-        node=unquote(node)
-        g.nodes[node]['category'].append(category)
-
-    return g
-
-fileAticle = open('data/articles.tsv', 'r')
-articlesLines = fileAticle.readlines()
-fileAticle.close()
-
-fileLinks = open('data/links.tsv', 'r')
-linksLines = fileLinks.readlines()
-fileLinks.close()
-
-fileCategories = open('data/categories.tsv', 'r')
-categoriesLines = fileCategories.readlines()
-fileCategories.close()
-
-g = nx.DiGraph()  # create empty graph
-g = createNodes(g,articlesLines)
-g = createEdges(g,linksLines)
-g = addCategory(g,categoriesLines)
+g=createGraph()
+print(g)
 
 # ------------------------------------------------------------
 def game_statistics(game, betw, in_deg, out_deg, pge_rnk, clos, index):
